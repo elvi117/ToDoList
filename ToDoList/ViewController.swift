7a7ff241eb44cc00dbf84206ed1824ca7d930d0a
Lukasz Matuszczak <Poznan>
2016-07-28 18:06:58 +0200

    catch values from realm

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import RealmSwift
+class ViewController: UIViewController, UITableViewDelegate {
 
-class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var myTableView: UITableView!
+   var arrayOfTasks = [Task]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+       
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        let realm = try! Realm()
+        arrayOfTasks = Array(realm.objects(Task.self))
+       myTableView.reloadData()
+
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfTasks.count
+    }
+    
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("toDoNameCell", forIndexPath: indexPath) as! toDoNameCell
+        cell.titleLabel.text = arrayOfTasks[indexPath.row].name
+        cell.dateLabel.text = arrayOfTasks[indexPath.row].date
 
+        
+ 
+        
+        return cell
+    }
+    
+     func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return .None
+    }
+    
+     func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+    
+     func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+//        let movedObject = self.data[sourceIndexPath.row]
+//        data.removeAtIndex(sourceIndexPath.row)
+//        data.insert(movedObject, atIndex: destinationIndexPath.row)
+//        NSLog("%@", "\(sourceIndexPath.row) => \(destinationIndexPath.row) \(data)")
+        // To check for correctness enable:  self.tableView.reloadData()
+    }
+
+
+    
+    
 }
 
