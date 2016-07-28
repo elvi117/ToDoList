e32ff8ef2107e8bd1cb208ae1e37c64439666554
Lukasz Matuszczak <Poznan>
2016-07-28 18:05:57 +0200

    Table Cell

---
+++ ToDoList/toDoNameCell.swift
@@ -0,0 +1,27 @@
+//
+//  toDoNameCell.swift
+//  ToDoList
+//
+//  Created by Lukasz Matuszczak on 28/07/16.
+//
+//
+
+import UIKit
+
+class toDoNameCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priorityImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
