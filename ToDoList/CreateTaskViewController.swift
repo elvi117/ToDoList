794558cfd23eae1949afddded7a3f6df7db00fd6
Lukasz Matuszczak <lukaszmatuszczak@Wojciechs-MacBook-Pro.local>
2016-07-29 07:49:39 +0200

    priority property

--- ToDoList/CreateTaskViewController.swift
+++ ToDoList/CreateTaskViewController.swift
@@ -11,26 +11,57 @@ import RealmSwift
 
 class CreateTaskViewController: UIViewController {
 
-    @IBAction func priorityButtonClicked(sender: AnyObject) {
+       @IBAction func priorityButtonClicked(sender: AnyObject) {
+    
+       let b = sender as! UIButton
+        setPriority = b.tag
     }
+    
+    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var dataPicker: UIDatePicker!
-    
-   
     @IBOutlet weak var hButton: UIButton!
     @IBOutlet weak var mButton: UIButton!
     @IBOutlet weak var lButton: UIButton!
+    
+    
     var priorityMatched = 1
+    var setPriority:Int{
+        get{return priorityMatched}
+        set{
+            priorityMatched = newValue
+            switch newValue {
+            case 1:
+                self.hButton.selected = true
+                self.mButton.selected = false
+                self.lButton.selected = false
+            case 2:
+                self.hButton.selected = false
+                self.mButton.selected = true
+                self.lButton.selected = false
+            case 3:
+                self.hButton.selected = false
+                self.mButton.selected = false
+                self.lButton.selected = true
+            default: break
+                
+            }
+        }}
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .Plain, target: self, action: #selector(save))
+        
+    self.mButton.selected = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     @IBAction func save(sender: AnyObject) {
         let task:Task = Task()
         task.name = nameTextField.text!
